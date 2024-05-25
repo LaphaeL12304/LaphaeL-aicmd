91ca2112aa4855eb1994f4fb0edfa0a1f2421e00
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-05-26 02:01:51 +0800

    chore: change tool flake back to nix-community/poetry2nix
    see https://github.com/nix-community/poetry2nix/pull/1661

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     poetry2nix = {
-      url = "github:DataEraserC/poetry2nix";
+      url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
