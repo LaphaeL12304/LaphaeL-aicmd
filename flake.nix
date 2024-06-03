c7b5b7a0e0f3408e592413bed631e4da9265a7db
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-06-03 19:01:07 +0800

    feat: add meta info in nix derive

--- flake.nix
+++ flake.nix
@@ -10,35 +10,49 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      in
-      {
-        packages = {
-          laphaelaicmd_linux = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.laphaelaicmd_linux;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
+    in {
+      packages = {
+        laphaelaicmd_linux = with pkgs; mkPoetryApplication {
+          projectDir = self;
+          meta = {
+            description = "Enable chat AI to execute commands on linux with feedback-loop for multi-step missions";
+            longDescription = ''
+              Enable chat AI to execute commands on linux with feedback-loop for multi-step missions
+            '';
+            homepage = "https://www.gnu.org/software/hello/manual/";
+            license = lib.licenses.agpl3Only;
+            platforms = lib.platforms.all;
+          };
         };
+        default = self.packages.${system}.laphaelaicmd_linux;
+      };
 
-        # Shell for app dependencies.
-        #
-        #     nix develop
-        #
-        # Use this shell for developing your app.
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.laphaelaicmd_linux ];
-        };
+      # Shell for app dependencies.
+      #
+      #     nix develop
+      #
+      # Use this shell for developing your app.
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [self.packages.${system}.laphaelaicmd_linux];
+      };
 
-        # Shell for poetry.
-        #
-        #     nix develop .#poetry
-        #
-        # Use this shell for changes to pyproject.toml and poetry.lock.
-        devShells.poetry = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
-        };
-      });
+      # Shell for poetry.
+      #
+      #     nix develop .#poetry
+      #
+      # Use this shell for changes to pyproject.toml and poetry.lock.
+      devShells.poetry = pkgs.mkShell {
+        packages = [pkgs.poetry];
+      };
+    });
 }
